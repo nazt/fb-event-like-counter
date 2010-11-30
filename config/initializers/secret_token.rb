109dc4df55680ffc4567d89c9c8a0375850c60de
Nat Weerawan <nat.wrw@gmail.com>
2010-11-30 16:50:28 +0700

    First commit with Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventLikeCounter::Application.config.secret_token = '3f9be502ea26aa0b5f498e70ba84bd781ab94c54e11f6dff3519ed99e3f31b25db607d54c334fd63f577749aae831d44f5473465405e87dacd0a801a7327a63f'
