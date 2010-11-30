0f097d133c2f792f4bc1f237e59854829438416d
Nat Weerawan <nat.wrw@gmail.com>
2010-11-30 17:35:06 +0700

    Remove default index and use home#index instead

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 EventLikeCounter::Application.routes.draw do
   match '/auth/:provider/callback', :to => 'sessions#create'
-  match ':controller(/:action(/:id(.:format)))(/)'
+  match ':controller(/:action(/:id(.:format)))'
+  root :to => "home#index"
 end
