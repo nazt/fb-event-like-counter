3bb456af28f2e4b80c2d4781f47cf6573f67334a
Nat Weerawan <nat.wrw@gmail.com>
2010-11-30 17:22:37 +0700

    Change facebook apikey and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'suM6kRY4QiHco9UoyjBn7w', 'ho0GPOHpn7n2iiYmZtB9SyVtRUT9OnZGGe5ZZQInU78'
-  provider :facebook, '125770127482918', 'e7f29e986d7570848c69257f3c03cb7e', {:scope => 'publish_stream,offline_access,email'}
+  provider :facebook, '169238796442849', '35275b5cba02a364d9e170e3968521e3', {:scope => 'user_events, rsvp_event'}
 end
