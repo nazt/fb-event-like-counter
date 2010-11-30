4a632d2d57482165858f0499d89281f308241df1
Nat Weerawan <nat.wrw@gmail.com>
2010-11-30 17:06:48 +0700

    Fix class name in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Todo235::Application.routes.draw do
+EventLikeCounter::Application.routes.draw do
   match '/auth/:provider/callback', :to => 'sessions#create'
   match ':controller(/:action(/:id(.:format)))'
 end
